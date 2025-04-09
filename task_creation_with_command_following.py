cedba7170fc4bb9e4b9853da466baac30c3d51c1
cbennudr <cbennudriti@hotmail.com>
2025-04-09 13:52:37 -0500

    Finishign up program creation

--- task_creation_with_command_following.py
+++ task_creation_with_command_following.py
@@ -177,29 +177,29 @@ class ScreenPrompter:  # Define the ScreenPrompter class
         return pixel_x, pixel_y  # Return pixel coordinates
 
     def create_and_open_new_python_program(self, command):
-        match = re.search(r"CREATE_SCRIPT\((.*)\)", command)  # Match command pattern
-        if match:  # If command matches
+        # match = re.search(r"CREATE_SCRIPT\((.*)\)", command)  # Match command pattern
+        # if match:  # If command matches
 
-            # Create filename
-            filename_timestamp = f"{datetime.datetime.now().strftime('%m-%d-%G_%H-%M')}" # get datetime of when program was created
-            # filename = filename_timestamp + "__" + match.group(1).strip('"\'')  # create filename by adding the passed in filename to the timestamp string
-            filename = filename_timestamp + "__" + "generated_script.py"  # create filename by adding the passed in filename to the timestamp string
-            
-            cmd_flow_dir = os.getcwd() # get absolute working dir
-            filepath = rf"{cmd_flow_dir}\output\scripts\{filename}" # get absolute path of the file
+        # Create filename
+        filename_timestamp = f"{datetime.datetime.now().strftime('%m-%d-%G_%H-%M')}" # get datetime of when program was created
+        # filename = filename_timestamp + "__" + match.group(1).strip('"\'')  # create filename by adding the passed in filename to the timestamp string
+        filename = filename_timestamp + "__" + "generated_script.py"  # create filename by adding the passed in filename to the timestamp string
+        
+        cmd_flow_dir = os.getcwd() # get absolute working dir
+        filepath = rf"{cmd_flow_dir}\output\scripts\{filename}" # get absolute path of the file
 
-            # Need to go through and replace all instances of "\" with "\\" to ensure json readibilty
-            esc_filepath = ""
-            for ch in filepath:
-                if ch == "\\": # if SINGLE backslash
-                    esc_filepath = esc_filepath + "\\\\" # add DOUBLE backslash to the new string
-                else: # if regular char just add it to the new string
-                    esc_filepath = esc_filepath + ch
+        # Need to go through and replace all instances of "\" with "\\" to ensure json readibilty
+        esc_filepath = ""
+        for ch in filepath:
+            if ch == "\\": # if SINGLE backslash
+                esc_filepath = esc_filepath + "\\\\" # add DOUBLE backslash to the new string
+            else: # if regular char just add it to the new string
+                esc_filepath = esc_filepath + ch
 
-            self.generated_script_filepath = esc_filepath
+        self.generated_script_filepath = esc_filepath
 
-        else:
-            raise ValueError(f"Invalid filename in:\n {command}")            
+        # else:
+        #     raise ValueError(f"Invalid filename in:\n {command}")            
         
         # Create the commands that need to be executed to create a new script file 
         cmds = f"""
@@ -218,31 +218,76 @@ class ScreenPrompter:  # Define the ScreenPrompter class
         ]
         ```
         """
+
+        print(f"Cmds to create and open a new script: {cmds}")
         # Execute the commands to create a script
         self.execute_commands(cmds)
+        print("Done creating a new script file\n")
+
+    def get_script_from_model(self):
+
+        return """
+def test(a):
+    print(a)
+test(5)
+        """
+
+    def cvt_program_to_cmds(self, program):
+        cmds_str = """\
+[
+"PRESS_KEY(esc)",
+        """
+
+        #HACK-y way of doing this but whatever
+
+        # Loop through each character in the program and add the command to type it to the array
+        ch_inx = -1
+        while ch_inx < len(program)-1:
+            ch_inx += 1
+
+            # Check for space
+            if program[ch_inx] == " ":
+                cmds_str += '\n"PRESS_KEY(space)",'
+            # Check for tab
+            elif program[ch_inx] == "\t":
+                cmds_str += '\n"PRESS_KEY(esc)",' # press esc first in case user has autocomplete on
+                cmds_str += '\n"PRESS_KEY(tab)",'
+            # Check for new line
+            elif program[ch_inx] == "\n":
+                cmds_str += '\n"PRESS_KEY(esc)",' # press esc first in case user has autocomplete on
+                cmds_str += '\n"PRESS_KEY(enter)",'
+            # Else
+            else:
+                cur_str = ""
+                while program[ch_inx].isalnum():
+                    cur_str += program[ch_inx]
+                    ch_inx += 1
+
+                cmds_str += f'\n"TYPE({cur_str})",'
+
+        cmds_str += '\n"PRESS_KEY(ctrl+s)"\n]' # save and and add closing bracket
+
+        return cmds_str
+
+
 
     def type_python_program(self):
-
-        #TODO: this will need to be changed to automatically generate the script
         
-        cmds = f"""
+        # Get the python program as a string
+        prog = self.get_script_from_model()
+        # Convert the program to command format
+        cmds = self.cvt_program_to_cmds(prog)
+
+        # Convert to format that self.execute_commands is expecting
+        str_cmds = f"""
             ```json
-            [
-                "TYPE(import numpy as np)",
-                "PRESS_KEY(esc)",
-                "PRESS_KEY(enter)",
-                "TYPE(l = np.array([5, 1, 6, 2, 0]))",
-                "PRESS_KEY(enter)",
-                "TYPE(l.sort())",
-                "PRESS_KEY(enter)",
-                "TYPE(print(l))",
-                "PRESS_KEY(enter)",
-                "PRESS_KEY(ctrl+s)"
-            ]
+            {cmds}
             ```
             """
+
         # Execute the commands to create a script
-        self.execute_commands(cmds)
+        self.execute_commands(str_cmds)
+        print("Done typing python program\n")
 
     def execute_command(self, command):  # Method to execute a command
         if command.startswith("MOVE_MOUSE"):  # Check if command is to move mouse
@@ -295,8 +340,6 @@ class ScreenPrompter:  # Define the ScreenPrompter class
 
             self.create_and_open_new_python_program(command)
             self.type_python_program()
-            
-            print("Done creating script")
 
         elif command.startswith("EXECUTE_SCRIPT"):
             # Create the commands that need to be executed to create a new script file 
@@ -653,5 +696,14 @@ if __name__ == '__main__':  # Main execution block
     print(f"Processing task: {IMG_PROMPT}")  # Print the task being processed
 
     screenPrompter = ScreenPrompter(API_KEY)  # Create an instance of ScreenPrompter
-    result = screenPrompter.sendRequest(IMG_PROMPT)  # Send request to the model
-    print(f"Command execution result: {result}")  # Print the result of command execution
+    # result = screenPrompter.sendRequest(IMG_PROMPT)  # Send request to the model
+    # print(f"Command execution result: {result}")  # Print the result of command execution
+
+    cmd = """
+        ```json
+        [
+            "CREATE_SCRIPT()"
+        ]
+        ```
+    """
+    screenPrompter.execute_commands(cmd)
